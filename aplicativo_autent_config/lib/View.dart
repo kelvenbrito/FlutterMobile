6d19c1566c9f652a8125d0134416485d84c2986f
kelvenbrito <kelvenbrito14@gmail.com>
2024-04-09 18:43:49 -0300

    09/04/24

--- aplicativo_autent_config/lib/View.dart
+++ aplicativo_autent_config/lib/View.dart
@@ -2,22 +2,31 @@ import 'package:aplicativo_autent_config/Controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-  @override 
+  @override
   State<HomePage> createState() => _HomePageState();
-  
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   final dbHelper = BancoDadosUsuario();
   final _formKey = GlobalKey<FormState>();
+  // Controllers para os campos de texto
+  TextEditingController _idController = TextEditingController();
+  TextEditingController _nomeController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _telefoneController = TextEditingController();
+  TextEditingController _enderecoController = TextEditingController();
+
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(title: Text('Login')),
-body: Container(),
+      appBar: AppBar(title: Text('Login')),
+      body: Container(
+         builder: (BuildContext context) {}
+      ),
     );
   }
 }
