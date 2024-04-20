e2c81733e5a63fc51e3138bd0298bc0b6f24ed3f
kelvenbrito <kelvenbrito14@gmail.com>
2024-04-19 21:16:09 -0300

    19/04/2024

--- sa3_mobile/lib/ViewLista.dart
+++ sa3_mobile/lib/ViewLista.dart
@@ -12,16 +12,17 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  //Atributos
+  //atributos
   late SharedPreferences _prefs;
   final String email;
   List<String> tasks = []; // Lista de tarefas
-  
-  final TextEditingController _controller =
-      TextEditingController(); // Controlador de texto para o campo de entrada de nova tarefa
-   
-  _ListaPageState({required this.email});
+  List<bool> taskChecked = []; // Lista de estados dos checkboxes
+  // Controlador de texto para o campo de entrada de nova tarefa
+  final TextEditingController _controller = TextEditingController();
+  bool _showOnlyChecked =
+      false; // Estado para controlar se mostrar apenas as tarefas marcadas
 
+  _ListaPageState({required this.email});
 
   //Métodos
   @override
@@ -33,96 +34,177 @@ class _ListaPageState extends State<ListaPage> {
   Future<void> _loadPreferences() async {
     _prefs = await SharedPreferences.getInstance();
     setState(() {
-      tasks = _prefs.getStringList('${email}tasks') ??
-          []; // Carrega as tarefas armazenadas ou uma lista vazia se não houver tarefas
+      // Carrega as tarefas armazenadas ou uma lista vazia se não houver tarefas
+      tasks = _prefs.getStringList('${email}tasks') ?? [];
+      taskChecked = List<bool>.generate(
+          tasks.length, (index) => false); // Inicializa com false
     });
   }
 
   Future<void> saveTasks() async {
-    SharedPreferences prefs = await SharedPreferences
-        .getInstance(); // Obtém as preferências compartilhadas
-    await prefs.setStringList('${email}tasks',
-        tasks); // Salva a lista de tarefas nas preferências compartilhadas
+    // Obtém as preferências compartilhadas
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // Salva a lista de tarefas nas preferências compartilhadas
+    await prefs.setStringList('${email}tasks', tasks);
   }
 
   // Método para editar a tarefa
-void _editTask(BuildContext context, int index) {
-  showDialog(
-    context: context,
-    builder: (context) {
-      String editedTask = tasks[index];
-      return AlertDialog(
-        title: Text('Editar Tarefa'),
-        content: TextField(
-          onChanged: (value) {
-            editedTask = value;
-          },
-          controller: TextEditingController(text: tasks[index]),
-          decoration: InputDecoration(
-            hintText: 'Digite a tarefa',
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              setState(() {
-                tasks[index] = editedTask;
-                saveTasks();
-                Navigator.of(context).pop();
-              });
+  void _editTask(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        String editedTask = tasks[index];
+        return AlertDialog(
+          title: Text('Editar Tarefa'),
+          content: TextField(
+            onChanged: (value) {
+              editedTask = value;
             },
-            child: Text('Salvar'),
+            controller: TextEditingController(text: tasks[index]),
+            decoration: InputDecoration(
+              hintText: 'Digite a tarefa',
+            ),
           ),
-        ],
-      );
-    },
-  );
-}
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  tasks[index] = editedTask;
+                  saveTasks();
+                  Navigator.of(context).pop();
+                });
+              },
+              child: Text('Salvar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteTask(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Confirmar exclusão'),
+          content: Text('Tem certeza de que deseja excluir esta tarefa?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false); // Cancela a exclusão
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  tasks.removeAt(index); // Remove a tarefa da lista
+                  taskChecked.removeAt(index); // Remove o estado correspondente
+                  saveTasks(); // Salva as tarefas atualizadas
+                  Navigator.of(context).pop(true); // Confirma a exclusão
+                });
+              },
+              child: Text('Confirmar'),
+            ),
+          ],
+        );
+      },
+    ).then((confirmed) {
+      if (confirmed == true) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Tarefa excluída com sucesso!')),
+        );
+      }
+    });
+  }
+
+  void _toggleShowOnlyChecked() {
+    setState(() {
+      _showOnlyChecked = !_showOnlyChecked;
+    });
+  }
+
+  void _logout() {
+    // Limpa as preferências de email
+    _prefs.remove('email');
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Logout realizado sucesso!')),
+    );
+    // Navega de volta para a página de login
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+      builder: (context) => LoginScreen(),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
-     bool checked = false;
+    List<String> filteredTasks = _showOnlyChecked
+        ? tasks.where((task) => taskChecked[tasks.indexOf(task)]).toList()
+        : tasks;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lista de Tarefas'), // Título da barra de aplicativos
+        title: Text('Lista de Tarefas'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: _logout,
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: tasks.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Checkbox(
-              value: checked,
-              onChanged: (bool? newvalue) {
-                setState(() {
-                  checked = newvalue!;
-                  saveTasks();
-                });
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text('Mostrar tarefas concluidas'),
+              Switch(
+                value: _showOnlyChecked,
+                onChanged: (value) {
+                  _toggleShowOnlyChecked();
+                },
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredTasks.length,
+              itemBuilder: (context, index) {
+                int originalIndex = tasks.indexOf(filteredTasks[index]);
+                return ListTile(
+                  leading: Checkbox(
+                    value: taskChecked[originalIndex],
+                    onChanged: (bool? newValue) {
+                      setState(() {
+                        taskChecked[originalIndex] = newValue ?? false;
+                        saveTasks();
+                      });
+                    },
+                  ),
+                  title: Text(filteredTasks[index]),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          _editTask(context, originalIndex);
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          _deleteTask(context, originalIndex);
+                        },
+                      ),
+                    ],
+                  ),
+                );
               },
             ),
-            
-            title: Text(tasks[index]),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: () {
-                  _editTask(context, index);
-                  },
-                ),
-                IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    setState(() {
-                      tasks.removeAt(index);
-                      saveTasks();
-                    });
-                  },
-                ),
-              ],
-            ),
-          );
-        },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -140,13 +222,21 @@ void _editTask(BuildContext context, int index) {
                 actions: <Widget>[
                   TextButton(
                     onPressed: () {
-                      setState(() {
-                        tasks.add(
-                            _controller.text); // Adiciona a nova tarefa à lista
-                        saveTasks(); // Salva as tarefas atualizadas
-                        _controller.clear(); // Limpa o campo de entrada
-                        Navigator.of(context).pop(); // Fecha o diálogo
-                      });
+                      if (_controller.text.trim().isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content:
+                                  Text('Por favor, insira uma tarefa válida')),
+                        );
+                      } else {
+                        setState(() {
+                          tasks.add(_controller.text);
+                          taskChecked.add(false);
+                          saveTasks();
+                          _controller.clear();
+                          Navigator.of(context).pop();
+                        });
+                      }
                     },
                     child: Text('Adicionar'),
                   ),
